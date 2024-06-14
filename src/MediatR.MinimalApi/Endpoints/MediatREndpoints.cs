431516c48236d0ec1536d2f41263266a320d1d33
Krzysztof Pajak <krzysztof.pajak@epoczta.pl>
2024-06-14 21:54:04 +0200

    Further improvements

--- src/MediatR.MinimalApi/Endpoints/MediatREndpoints.cs
+++ src/MediatR.MinimalApi/Endpoints/MediatREndpoints.cs
@@ -2,6 +2,7 @@
 using MediatR.MinimalApi.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -71,12 +72,47 @@ namespace MediatR.MinimalApi.Endpoints
 
 
         private static object? CreateFromQueryRequest(Type requestType, HttpRequest httpRequest)
-        {            
-            var request = Activator.CreateInstance(requestType);
+        {
+            var parameterValues = ExtractParameterValues(requestType, httpRequest.Query);
+            var request = Activator.CreateInstance(requestType, parameterValues);
             PopulateRequestFromQuery(request, httpRequest.Query);
             return request;
         }
 
+        private static object?[]? ExtractParameterValues(Type requestType, IQueryCollection query)
+        {
+            var constructor = requestType.GetConstructors().FirstOrDefault();
+            if (constructor == null)
+            {
+                return default;
+            }
+
+            var parameters = constructor.GetParameters();
+            var args = new object?[parameters.Length];
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                var param = parameters[i];
+                if (query.TryGetValue(param.Name!, out var value))
+                {
+                    args[i] = ConvertToType(value.ToString(), param.ParameterType);
+                }
+                else
+                {
+                    args[i] = GetDefaultValue(param.ParameterType);
+                }
+            }
+
+            return args;
+        }
+        private static object? GetDefaultValue(Type type)
+        {
+            if (type.IsValueType)
+            {
+                return Activator.CreateInstance(type);
+            }
+            return null;
+        }
         private static async ValueTask<object?> CreateFromBodyRequest(Type requestType, HttpRequest httpRequest)
         {
             return httpRequest.ContentLength is > 0 ? await httpRequest.ReadFromJsonAsync(requestType) : Activator.CreateInstance(requestType);
@@ -85,15 +121,47 @@ namespace MediatR.MinimalApi.Endpoints
         private static void PopulateRequestFromQuery(object? request, IQueryCollection query)
         {
             if (request == null) return;
-            
             var requestType = request.GetType();
+
             foreach (var property in requestType.GetProperties())
             {
-                if (!query.ContainsKey(property.Name)) continue;
-                
-                var value = query[property.Name].ToString();
-                var convertedValue = Convert.ChangeType(value, property.PropertyType);
-                property.SetValue(request, convertedValue);
+                if (property.GetCustomAttribute<FromQueryAttribute>() is not null && query.TryGetValue(property.Name, out var value))
+                {
+                    var convertedValue = Convert.ChangeType(value.ToString(), property.PropertyType);
+                    property.SetValue(request, convertedValue);
+                }
+            }
+
+        }
+        private static object? ConvertToType(string value, Type type)
+        {
+            if (type == typeof(Guid))
+            {
+                return Guid.Parse(value);
+            }
+            else if (type == typeof(int))
+            {
+                return int.Parse(value);
+            }
+            else if (type == typeof(long))
+            {
+                return long.Parse(value);
+            }
+            else if (type == typeof(bool))
+            {
+                return bool.Parse(value);
+            }
+            else if (type == typeof(DateTime))
+            {
+                return DateTime.Parse(value);
+            }
+            else if (type.IsEnum)
+            {
+                return Enum.Parse(type, value);
+            }
+            else
+            {
+                return Convert.ChangeType(value, type);
             }
         }
     }
--- src/MediatR.MinimalApi/Extensions/OpenApiExtensions.cs
+++ src/MediatR.MinimalApi/Extensions/OpenApiExtensions.cs
@@ -58,24 +58,43 @@ namespace MediatR.MinimalApi.Extensions
 
         private static OpenApiParameter[] ExtractParameters(Type requestType)
         {
+            var propertyParameters = GetPropertyParameters(requestType);
+            var constructorParameters = GetConstructorParameters(requestType);
+
+            return propertyParameters.Concat(constructorParameters).ToArray();
+        }
+        private static IEnumerable<OpenApiParameter> GetPropertyParameters(Type requestType)
+        {
             return requestType.GetProperties()
                 .Where(property => property.GetCustomAttribute<FromQueryAttribute>() is not null)
-                .Select(property =>
-                {
-                    var name = property.Name;
-                    var description = property.GetCustomAttribute<DescriptionAttribute>()?.Description ?? string.Empty;
-                    var parameterType = property.PropertyType;
+                .Select(property => CreateOpenApiParameter(property.Name, property.PropertyType, property.GetCustomAttributes()));
+        }
+        private static IEnumerable<OpenApiParameter> GetConstructorParameters(Type requestType)
+        {
+            var constructor = requestType.GetConstructors().FirstOrDefault();
 
-                    return new OpenApiParameter
-                    {
-                        Name = name,
-                        Description = description,
-                        In = ParameterLocation.Query,
-                        Required = property.GetCustomAttribute<RequiredAttribute>() != null,
-                        Schema = new OpenApiSchema { Type = parameterType.Name.ToLower() }
-                    };
-                })
-                .ToArray();
+            if (constructor == null)
+            {
+                return Enumerable.Empty<OpenApiParameter>();
+            }
+
+            return constructor.GetParameters()
+                .Where(param => param.GetCustomAttribute<FromQueryAttribute>() is not null)
+                .Select(param => CreateOpenApiParameter(param.Name!, param.ParameterType, param.GetCustomAttributes()));
+        }
+        private static OpenApiParameter CreateOpenApiParameter(string name, Type parameterType, IEnumerable<Attribute> attributes)
+        {
+            var description = attributes.OfType<DescriptionAttribute>().FirstOrDefault()?.Description ?? string.Empty;
+            var isRequired = attributes.OfType<RequiredAttribute>().Any();
+
+            return new OpenApiParameter
+            {
+                Name = name,
+                Description = description,
+                In = ParameterLocation.Query,
+                Required = isRequired,
+                Schema = new OpenApiSchema { Type = parameterType.Name.ToLower() }
+            };
         }
 
         private static OpenApiRequestBody CreateRequestBody(Type requestType)
@@ -83,15 +102,15 @@ namespace MediatR.MinimalApi.Extensions
             return new OpenApiRequestBody
             {
                 Content = new Dictionary<string, OpenApiMediaType>
-            {
                 {
-                    MediaTypeNames.Application.Json,
-                    new OpenApiMediaType
                     {
-                        Schema = new OpenApiSchema { Type = requestType.Name }
+                        MediaTypeNames.Application.Json,
+                        new OpenApiMediaType
+                        {
+                            Schema = new OpenApiSchema { Type = requestType.Name }
+                        }
                     }
                 }
-            }
             };
         }
 
@@ -113,107 +132,11 @@ namespace MediatR.MinimalApi.Extensions
         private static OpenApiResponses CreateResponses(Type responseType)
         {
             return new OpenApiResponses
-        {
-            {
-                ((int)HttpStatusCode.OK).ToString(),
-                new OpenApiResponse
-                {
-                    Description = "Success",
-                    Content = new Dictionary<string, OpenApiMediaType>
-                    {
-                        {
-                            MediaTypeNames.Application.Json,
-                            new OpenApiMediaType
-                            {
-                                Schema = new OpenApiSchema { Type = responseType.Name }
-                            }
-                        }
-                    }
-                }
-            }
-        };
-    }
-
-        /*public static void WithOpenApiDescription(this RouteHandlerBuilder builder, Type requestType)
-        {
-            var attribute = requestType.GetCustomAttribute<EndpointAttribute>();
-
-            var responseType = requestType.GetInterfaces()
-                .First(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequest<>))
-                .GetGenericArguments()[0];
-
-            var operation = new OpenApiOperation();
-            operation.Summary = $"Operation for {requestType.Name}";
-            operation.Description = $"Handles requests for {requestType.Name}";
-            operation.Parameters = requestType.GetProperties().Select(property =>
-            {
-                if (property.GetCustomAttribute<FromQueryAttribute>() is not null)
-                {
-                    var name = property.Name;
-                    var description = property.GetCustomAttribute<DescriptionAttribute>()?.Description ?? string.Empty;
-                    var parameterType = property.PropertyType;
-                    return new OpenApiParameter
-                    {
-                        Name = name,
-                        Description = description,
-                        In = ParameterLocation.Query,
-                        Required = property.GetCustomAttribute<RequiredAttribute>() != null,
-                        Schema = new OpenApiSchema
-                        {
-                            Type = parameterType.Name.ToLower()
-                        }
-                    };
-                }
-                return null;
-            }).Where(x => x is not null).ToArray();
-
-            if (!string.IsNullOrEmpty(attribute!.TagName))
-            {
-                operation.Tags = new List<OpenApiTag> {
-                    new OpenApiTag
-                    {
-                        Name = attribute.TagName
-                    }
-                };
-            }
-            if (attribute.Method != Models.HttpMethod.GET)
-            {
-                operation.RequestBody = new OpenApiRequestBody
-                {
-                    Content = new Dictionary<string, OpenApiMediaType>
-                    {
-                        {
-                            MediaTypeNames.Application.Json,
-                            new OpenApiMediaType
-                            {
-                                Schema = new OpenApiSchema
-                                {
-                                    Type = requestType.Name
-                                }
-                            }
-                        }
-                    }
-                };
-                builder.WithMetadata(new AcceptsMetadata(["application/json"], requestType));
-            }
-            var producesResponseTypeAttribute = requestType.GetCustomAttributes<ProducesResponseTypeAttribute>();
-            if (producesResponseTypeAttribute.Any())
-            {
-                producesResponseTypeAttribute.ToList().ForEach(producesResponseType =>
-                {
-                    builder.WithMetadata(new ProducesResponseTypeMetadata(producesResponseType.StatusCode, producesResponseType.Type));
-                });
-            }
-            else
-            {
-                builder.WithMetadata(new ProducesResponseTypeMetadata(200, responseType));
-            }
-            operation.Responses = new()
             {
                 {
                     ((int)HttpStatusCode.OK).ToString(),
-                    new OpenApiResponse()
-                   {
+                    new OpenApiResponse
+                    {
                         Description = "Success",
                         Content = new Dictionary<string, OpenApiMediaType>
                         {
@@ -221,20 +144,13 @@ namespace MediatR.MinimalApi.Extensions
                                 MediaTypeNames.Application.Json,
                                 new OpenApiMediaType
                                 {
-                                    Schema = new OpenApiSchema
-                                    {
-
-                                        Type = responseType.Name,
-                                    }
+                                    Schema = new OpenApiSchema { Type = responseType.Name }
                                 }
                             }
                         }
                     }
                 }
             };
-            
-
-            builder.WithMetadata(operation);
-        }*/
+        }
     }
 }
