05b0b4775b253ee07c90e044b53dab44182d5605
Krzysztof Pajak <krzysztof.pajak@epoczta.pl>
2024-06-14 21:54:32 +0200

    Sample - add GetUserByIdQuery

---
+++ samples/MediatRSampleWebApplication/Queries/Users/GetUserByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using MediatR.MinimalApi.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace MediatRSampleWebApplication.Queries.Users;
+
+[Endpoint("user/{id}", MediatR.MinimalApi.Models.HttpMethod.GET, "User")]
+public record GetUserByIdQuery([FromQuery] Guid UserId) : IRequest<User>;
---
+++ samples/MediatRSampleWebApplication/Queries/Users/GetUserByIdQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using WebApplication.Models;
+
+namespace MediatRSampleWebApplication.Queries.Users;
+
+public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, User>
+{
+    public async Task<User> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await Task.FromResult(new User("John", "Doe", "email@email.com") { Id = request.UserId });
+    }
+}
