66aee75068e6657d9efc6d096fd60f058a328ef3
Krzysztof Pajak <krzysztof.pajak@epoczta.pl>
2024-06-24 20:12:49 +0200

    Change AddAuthorization to internal

--- src/MediatR.MinimalApi/Extensions/EndpointAuthorizationExtensions.cs
+++ src/MediatR.MinimalApi/Extensions/EndpointAuthorizationExtensions.cs
@@ -6,7 +6,7 @@ namespace MediatR.MinimalApi.Extensions;
 
 internal static class EndpointAuthorizationExtensions
 {
-    public static RouteHandlerBuilder AddAuthorization(this RouteHandlerBuilder builder, Type handlerType)
+    internal static RouteHandlerBuilder AddAuthorization(this RouteHandlerBuilder builder, Type handlerType)
     {
         var authorizeAttributes = handlerType.GetCustomAttributes()
                 .Where(attr => attr.GetType() == typeof(AuthorizeAttribute) || attr.GetType() == typeof(AllowAnonymousAttribute))
