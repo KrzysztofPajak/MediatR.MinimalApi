59d9b62f307fd154eb27c77428080d5a92053777
Krzysztof Pajak <krzysztof.pajak@epoczta.pl>
2024-06-26 19:18:44 +0200

    Sample project - Add delete user command

---
+++ samples/MediatRSampleWebApplication/Commands/Users/DeleteUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using MediatR.MinimalApi.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using HttpMethod = Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http.HttpMethod;
+
+namespace WebApplication.Commands.Users;
+
+[Endpoint("user/Delete/{Id}", HttpMethod.Delete, "User")]
+public record DeleteUserCommand([FromRoute] Guid Id) : IRequest<bool>;
