f0ce10e0fdeb4a4d062d6d99149663c374280131
KrzysztofPajak <krzysztof.pajak@epoczta.pl>
2024-10-19 19:54:22 +0200

    Minor changes

--- samples/MediatRSampleWebApplication/Commands/Companies/CreateCompany.cs
+++ samples/MediatRSampleWebApplication/Commands/Companies/CreateCompany.cs
@@ -3,22 +3,21 @@ using MediatRSampleWebApplication.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MediatRSampleWebApplication.Commands.Companies
-{
-    public static class CreateCompany
-    {        
-        public record CreateCompanyRequest(string Name);
+namespace MediatRSampleWebApplication.Commands.Companies;
 
-        [Authorize]
-        public record CreateCompanyCommand([FromBody] CreateCompanyRequest Company) : IRequest<Company>;
+public static class CreateCompany
+{        
+    public record CreateCompanyRequest(string Name);
 
-        public class Handler : IRequestHandler<CreateCompanyCommand, Company>
+    [Authorize]
+    public record CreateCompanyCommand([FromBody] CreateCompanyRequest Company) : IRequest<Company>;
+
+    public class Handler : IRequestHandler<CreateCompanyCommand, Company>
+    {
+        public Task<Company> Handle(CreateCompanyCommand request, CancellationToken cancellationToken)
         {
-            public Task<Company> Handle(CreateCompanyCommand request, CancellationToken cancellationToken)
-            {
-                var company = new Company { Name = request.Company.Name, Balance = 0 };
-                return Task.FromResult(company);
-            }
+            var company = new Company { Name = request.Company.Name, Balance = 0 };
+            return Task.FromResult(company);
         }
     }
 }
--- samples/MediatRSampleWebApplication/Commands/Roles/GetRoles.cs
+++ samples/MediatRSampleWebApplication/Commands/Roles/GetRoles.cs
@@ -1,24 +1,22 @@
 ﻿using MediatR;
 using MediatRSampleWebApplication.Models;
 
-namespace MediatRSampleWebApplication.Commands.Roles
+namespace MediatRSampleWebApplication.Commands.Roles;
+public static class GetRoles
 {
-    public static class GetRoles
+    public record GetRolesCommand : IRequest<IList<Role>>;
+
+    public class GetRolesCommandHandler : IRequestHandler<GetRolesCommand, IList<Role>>
     {
-        public record GetRolesCommand : IRequest<IList<Role>>;
-
-        public class GetRolesCommandHandler : IRequestHandler<GetRolesCommand, IList<Role>>
+        public Task<IList<Role>> Handle(GetRolesCommand request, CancellationToken cancellationToken)
         {
-            public Task<IList<Role>> Handle(GetRolesCommand request, CancellationToken cancellationToken)
+            var roles = new List<Role>
             {
-                var roles = new List<Role>
-                {
-                    new Role("Test1", Guid.NewGuid()),
-                    new Role("Test2", Guid.NewGuid())
-                };
+                new Role("Test1", Guid.NewGuid()),
+                new Role("Test2", Guid.NewGuid())
+            };
 
-                return Task.FromResult<IList<Role>>(roles);
-            }
+            return Task.FromResult<IList<Role>>(roles);
         }
     }
 }
